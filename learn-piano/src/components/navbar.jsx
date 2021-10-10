ffc7479a11c03c9c3ec9d33951f4d754ecdb2a33
Samuel Ingledue <saingledue@outlook.com>
2021-10-10 16:11:49 -0500

    set up a navbar

---
+++ learn-piano/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import styles from './styles/navbar.css';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <ul>
+                    <li><a class="active" href="#home">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </React.Fragment>
+        );
+    }
+}
+
+export default NavBar;
